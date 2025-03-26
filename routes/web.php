3f2531b525e67137e1ad682ae7d51142b2e464ac
Daniel <u2_danhru_waw@technischoosl>
2025-03-26 17:43:23 +0100

    Routes Reloaded

--- routes/web.php
+++ routes/web.php
@@ -1,104 +1,28 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-// Главная
+// Home
 
-Route::get('/', function () {
-    // $jobs = Job::all();
-    // dd($jobs[0]);
-    return view('home');
-});
+Route::view('/', 'home');
 
-// Контакты
+// Contact
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
 
+// Route::controller(JobController::class)->group(function(){
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
 
-// Все job с employer и нумерацией страниц
+Route::resource('jobs', JobController::class);
 
-Route::get('/jobs', function (){
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-    
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-//Адрессация на создание job
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Каждый отдельный job по id
-
-Route::get('/jobs/{id}', function ($id){
-
-    $job = Job::find($id);
-    return view('jobs.show', ['job' => $job]);
-});
-
-// Создание job
-Route::post('/jobs', function () {
-    // dd('Hello from the post');
-    // dd(request()->all());
-
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required'],
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Редактируй
-Route::get('/jobs/{id}/edit', function ($id){
-
-    $job = Job::find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-// Обновить
-
-Route::patch('/jobs/{id}', function ($id){
-
-    //Validation
-
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    //authorize
-
-    $job = Job::findOrFail($id); // null if we cant find
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary'),
-    ]); 
-
-    //redirect
-    return redirect('/jobs/' . $job->id);   
-});
-
-// remove
-
-Route::delete('/jobs/{id}', function ($id){
-    Job::findOrFail($id)->delete();
-
-    return redirect('/jobs');
-});
 
 
 
