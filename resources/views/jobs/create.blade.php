a35658043fb51b049eb04a9e93f846110acd2fbf
Daniel <u2_danhru_waw@technischoosl>
2025-04-07 15:34:54 +0200

    Final commit

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot:heading>
-        Create Job 
+        Create Job
     </x-slot:heading>
 
     <form method="POST" action="/jobs">
@@ -9,7 +9,7 @@
           <div class="border-b border-gray-900/10 pb-12">
             <h2 class="text-base/7 font-semibold text-gray-900">Create a New Job</h2>
             <p class="mt-1 text-sm/6 text-gray-600">Give a information</p>
-      
+
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
               <x-form-field class="sm:col-span-4">
@@ -27,18 +27,18 @@
                 </div>
                 <x-form-error name="salary"></x-form-error>
               </x-form-field >
-              
+
             </div>
           </div>
         </div>
-      
+
         <div class="mt-6 flex items-center justify-end gap-x-6">
           <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
           <x-form-button>
             Save
           </x-form-button>
-          
+
         </div>
       </form>
-      
+
 </x-layout>
